cbaa3e451142585a00016ebefb9fd4aae580bca2
Link PC <link.dovakin@gmail.com>
2024-09-22 19:08:30 +0800

    新增AuraCharacter、AuraGoblin的蓝图，并赋予了武器设置

--- Source/Aura/Private/character/AuraCharacterBase.cpp
+++ Source/Aura/Private/character/AuraCharacterBase.cpp
@@ -6,9 +6,11 @@
 // Sets default values
 AAuraCharacterBase::AAuraCharacterBase()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), "WeaponHandSocket");
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
