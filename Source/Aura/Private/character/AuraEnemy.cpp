181333bde9d14564a03ab761f6f6d5852096968f
Link PC <link.dovakin@gmail.com>
2024-09-25 15:43:58 +0800

    包含武器的高亮逻辑

--- Source/Aura/Private/character/AuraEnemy.cpp
+++ Source/Aura/Private/character/AuraEnemy.cpp
@@ -3,6 +3,8 @@
 
 #include "character/AuraEnemy.h"
 
+#include "Aura/Aura.h"
+
 AAuraEnemy::AAuraEnemy()
 {
 	// Ensure the mesh component is set up correctly
@@ -15,13 +17,15 @@ void AAuraEnemy::HighlightActor()
 {
 	bHighlighted = true;
 	GetMesh()->SetRenderCustomDepth(true);
-	GetMesh()->SetCustomDepthStencilValue(250);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	bHighlighted = false;
 	GetMesh()->SetRenderCustomDepth(false);
-	GetMesh()->SetCustomDepthStencilValue(0);
+	Weapon->SetRenderCustomDepth(false);
 }
 
