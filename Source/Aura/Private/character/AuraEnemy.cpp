573705762fee3bc368f327bdc98a24b7c39c5bf5
link.dovakin <link.dovakin@gmail.com>
2024-09-25 21:46:30 +0800

    优化AuraEnemy，增加指针非空判断

--- Source/Aura/Private/character/AuraEnemy.cpp
+++ Source/Aura/Private/character/AuraEnemy.cpp
@@ -18,14 +18,20 @@ void AAuraEnemy::HighlightActor()
 	bHighlighted = true;
 	GetMesh()->SetRenderCustomDepth(true);
 	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
-	Weapon->SetRenderCustomDepth(true);
-	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(true);
+		Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	}
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	bHighlighted = false;
 	GetMesh()->SetRenderCustomDepth(false);
-	Weapon->SetRenderCustomDepth(false);
+	if (Weapon)
+	{
+		Weapon->SetRenderCustomDepth(false);
+	}
 }
 
